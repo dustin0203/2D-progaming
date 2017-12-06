8fadb8d90b9d31b2edd82c4900a2174aecae8bf1
dustin0203 <32826154+dustin0203@users.noreply.github.com>
2017-12-06 15:05:13 +0900

    main_state

--- main_state.py
+++ main_state.py
@@ -3,43 +3,56 @@ import json
 import os
 
 from pico2d import *
-from GameObject import *
+
+from debris import *
+from background import Background
+from building1 import Building1
+from building2 import Building2
+from building3 import Building3
+from player import Player
+from ui import UI
 
 import game_framework
 import title_state
-
 
 name = "MainState"
 
 player = None
 background = None
 buildings = None
-Brokenbuildings = None
-
+wreckages = None
 camerRect = None
+ui = None
+count = 0
+state = 0
 
 def enter():
-    global player, background, buildings,camerRect,Brokenbuildings
-    background = Background("background.png")
-    buildings = [Building("building.png",130,200) for i in range(1)]
-    Brokenbuildings = []
+    global player,background,buildings,camerRect,wreckages,ui,state
 
-    player = Player("player.png", 320, 1270, 5, 5)
+    background = Background()
+    buildings = []
+    wreckages = []
+    ui = UI()
+
+    player = Player(320, 1270)
     camerRect = SDL_Rect(int(0),int(0),int(640),int(480))
 
     game_framework.reset_time()
 
+    state = 0
 
 def exit():
-    global player, background,buildings,camerRect,Brokenbuildings
+    global player,background,buildings,camerRect,wreckages,ui
+    ui.Record_score()
     del(player)
     del(background)
     del(buildings)
     del(camerRect)
-    del(Brokenbuildings)
+    del(wreckages)
+    del(ui)
+
 def pause():
     pass
-
 
 def resume():
     pass
@@ -54,13 +67,15 @@ def handle_events(frame_time):
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.change_state(title_state)
         else:
-
             player.handle_event(event)
 
-
 def update(frame_time):
-    global player, background, buildings,camerRect,Brokenbuildings
 
+    global player,background,buildings,camerRect,wreckages,ui,count,state
+
+    r = random.randint(0,2)
+
+    count += frame_time
 
     player.Update(frame_time)
 
@@ -69,47 +84,70 @@ def update(frame_time):
         player.IntersectsWith(building)
 
     for building in buildings:
-        if(building.HIT):
-             Brokenbuildings.append(Brokenbuilding(int(building.GetOriginX()),int(building.GetOriginY())))
+        if(building.Broken_on == True):
+            wreckages.append(Wreckage(int(building.GetOriginX()), int(building.GetOriginY())))
+            building.Broken_on = False
+            player.SCORE += building.SCORE
+            if (player.SKILL_value < 100):
+                player.SKILL_value += 10
+
         if(building.Broken):
             buildings.remove(building)
-            buildings.append(Building("building.png",130,100))
+            if state == 1:
+                state = 0
+                if player.SKILL == True:
+                    player.positionRect.y = 1270
+                    player.SKILL = False
+            #buildings.append(Building1("building1.png", 130, 100))
 
+    if state == 0:
+        if int(count % 8) == 0:
+            if r == 0:
+                buildings.append(Building1(130, -500))
+                count += 1
+            if r == 1:
+                buildings.append(Building2(180,-500))
+                count += 1
+            if r == 2:
+                buildings.append(Building3(70,-500))
+                count += 1
 
-
-    for Broken in Brokenbuildings:
+    for Broken in wreckages:
         Broken.Update(frame_time)
         if(Broken.remove):
-            Brokenbuildings.remove(Broken)
-
+            wreckages.remove(Broken)
 
     camerRect.x = abs(int(player.GetOriginX()-320))
     camerRect.y = abs(int(player.GetOriginY()-240))
-
 
     if(camerRect.y <0):
             camerRect.y= 0
     if(camerRect.y + camerRect.h>=background.GetHeight()):
             camerRect.y = background.GetHeight() - 480
 
+    ui.Update(player)
 
+    if (player.HP == 0):
+        game_framework.change_state(title_state)
 
 def draw(frame_time):
-    global player, background, buildings,camerRect,Brokenbuildings
+
+    global player, background, buildings, camerRect, wreckages, ui, state
 
     clear_canvas()
 
+    if state == 0:
+        background.Draw(camerRect)
 
-    background.Draw(camerRect)
 
     for building in buildings:
         building.Draw(camerRect)
 
-    for Broken in Brokenbuildings:
+    for Broken in wreckages:
         Broken.Draw(camerRect)
 
+
     player.Draw(camerRect)
+    ui.Draw()
 
     update_canvas()
-
-
